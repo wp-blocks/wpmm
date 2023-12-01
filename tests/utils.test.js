c738146f274cab75ae7940528b03a5bd6df64277
Erik Golinelli <erik@codekraft.it>
2023-12-01 21:15:25 +0100

    Add additional utility functions and corresponding tests
    Extended utility functions in `utils.js` to include `generateSalt`, `replaceDbConstant`, `replaceDbConstantBool`, and `getDownloadUrl`. These additional functions increase the module's flexibility in manipulating and fetching WordPress data. Included corresponding tests in `utils.test.js` to ensure the correctness and reliability of these new functions.

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,4 @@
-const {getConfig, getWordPressDownloadUrl} = require('../lib/utils.js');
+const { getConfig, getWordPressDownloadUrl, generateSalt, replaceDbConstant, replaceDbConstantBool, getDownloadUrl } = require('../lib/utils.js');
 const defaultConfig = 'wp-package.json';
 
 describe('getConfig', () => {
@@ -9,7 +9,6 @@ describe('getConfig', () => {
     expect(config).toBeInstanceOf(Object);
   });
 });
-
 
 describe('getWordPressDownloadUrl', () => {
   test('returns English URL when language is not provided', () => {
@@ -22,5 +21,64 @@ describe('getWordPressDownloadUrl', () => {
 
   test('returns language-specific URL when language does not start with "en"', () => {
     expect(getWordPressDownloadUrl('5.7.1', 'it_IT')).toBe('https://it.wordpress.org/wordpress-5.7.1-it_IT.zip');
+  });
+});
+
+describe('generateSalt', () => {
+  test('should return a string', () => {
+    const salt = generateSalt();
+    expect(typeof salt).toBe('string');
+  });
+
+  test('should return a string of length 64', () => {
+    const salt = generateSalt();
+    expect(salt.length).toBe(64);
+  });
+
+  test('should only contain valid characters', () => {
+    const validChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/';
+    const salt = generateSalt();
+    const isAllValidChars = Array.from(salt).every(char => validChars.includes(char));
+    expect(isAllValidChars).toBe(true);
+  });
+});
+
+// Jest Unit Tests
+test('replaceConstant', () => {
+  const content = "define( 'NAME', 'oldValue' );";
+  const content2 = "define( 'BOOL', TRUE );";
+  const name = 'NAME';
+  const name2 = 'BOOL';
+  const value = 'newValue';
+  const value2 = 'FALSE';
+  const expectedContent = "define( 'NAME', 'newValue' );";
+  const expectedContent2 = "define( 'BOOL', FALSE );";
+
+  expect(replaceDbConstant(content, name, value)).toBe(expectedContent);
+  expect(replaceDbConstantBool(content2, name2, value2)).toBe(expectedContent2);
+});
+
+describe('getDownloadUrl', () => {
+  test('should return absolute url if present', () => {
+    expect(getDownloadUrl('http://test.com', '1.0', 'plugins')).toEqual('http://test.com');
+    expect(getDownloadUrl('https://test.com', '1.0', 'plugins')).toEqual('https://test.com');
+  });
+
+  test('should return url with version if present', () => {
+    expect(getDownloadUrl('test', '1.0', 'plugins')).toEqual('https://downloads.wordpress.org/plugins/test.1.0.zip');
+  });
+
+  test('should return url without version if not present', () => {
+    expect(getDownloadUrl('test', undefined, 'plugins')).toEqual('https://downloads.wordpress.org/plugins/test.zip');
+  });
+
+  test('should return url for different types', () => {
+    expect(getDownloadUrl('test', '1.0', 'themes')).toEqual('https://downloads.wordpress.org/themes/test.1.0.zip');
+    expect(getDownloadUrl('test', '1.0', 'plugins')).toEqual('https://downloads.wordpress.org/plugins/test.1.0.zip');
+  });
+
+  test('should return url for different package names', () => {
+    expect(getDownloadUrl('test1', '1.0', 'plugins')).toEqual('https://downloads.wordpress.org/plugins/test1.1.0.zip');
+    expect(getDownloadUrl('test2', '2.0', 'plugins')).toEqual('https://downloads.wordpress.org/plugins/test2.2.0.zip');
   });
 });
