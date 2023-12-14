7ffe5d08f5823d47471ace2928e65882086ed513
Erik Golinelli <erik@codekraft.it>
2023-12-14 14:42:46 +0100

    Create Axios utility file for API calls
    This commit introduces a new utility file for handling axios API calls, with configured interceptors and request options. It also includes a getTemplate and a getWpVersionCheck function that make use of the axiosFetch helper to retrieve data.

---
+++ src/utils/axios.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { WPapiCoreVersionResponse, WPapiTemplateResponse } from '../types'
+
+axios.interceptors.response.use(
+    (res) => res,
+    (error: AxiosError) => {
+        const { data, status, config } = error.response!;
+        switch (status) {
+            case 400:
+                console.error(data, config);
+                break;
+
+            case 401:
+                console.error('unauthorised');
+                break;
+
+            case 404:
+                console.error('/not-found');
+                break;
+
+            case 500:
+                console.error('/server-error');
+                break;
+        }
+        return Promise.reject(error);
+    }
+);
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const reqOptions = {
+    headers: {
+        'User-Agent':
+            'WPMM - WordPress Package Manager https://github.com/wp-blocks/wpmm',
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Connection: 'keep-alive',
+        Pragma: 'no-cache',
+    },
+}
+
+const axiosFetch = {
+    get: <T>(url: string) => axios.get<T>(url, reqOptions).then(responseBody),
+    post: <T>(url: string, body: object) =>
+        axios.post<T>(url, body, reqOptions).then(responseBody),
+};
+
+/**
+ * Retrieves a template using the provided URL.
+ *
+ * @param url - The URL of the template.
+ * @return A promise that resolves to the template data.
+ */
+export const getTemplate = (url: string): Promise<{ data: WPapiTemplateResponse }> =>
+    axiosFetch.get<{ data: WPapiTemplateResponse }>(url)
+
+
+/**
+ * Fetch the WordPress version from WordPress.org
+ */
+export const getWpVersionCheck = (): Promise<WPapiCoreVersionResponse> =>
+    axiosFetch.get<WPapiCoreVersionResponse>('https://api.wordpress.org/core/version-check/1.7/')
+
+export default axiosFetch
