f6b478a46773d0be8116d346446c91e5ac34f306
Erik Golinelli <erik@codekraft.it>
2023-12-14 14:47:01 +0100

    Add `constants.ts` for WordPress default settings
    Introduced a new `constants.ts` file that contains the default settings and configurations for WordPress installations. This encapsulates constants like default installation folder, language, and database settings, making it easier to manage and modify default parameters across the application.

---
+++ src/constants.ts
@@ -0,0 +1,37 @@
+import { WPMMconfigWP } from './types'
+
+/**
+ * the name of the WordPress package.json file
+ */
+export const PkgFileName: string = 'wp-package.json'
+
+/**
+ * the default folder name for the WordPress install
+ * @type {string} defaultWpInstallFolder - The default folder name for the WordPress install
+ */
+export const DefaultWpInstallFolder: string = 'wordpress'
+
+export const DefaultWpDatabaseType: string = 'mysql'
+
+/**
+ * the default language for the WordPress install
+ *
+ * @type {string} defaultWpInstallLanguage - The default language for the WordPress install
+ */
+export const DefaultWpInstallLanguage: string = 'en_US'
+
+/**
+ * The default configuration object
+ *
+ * @type {WPMMconfigWP} defaultConfig - The default configuration object
+ */
+export const DefaultWpConfig: WPMMconfigWP = {
+    DB_NAME: 'my_db_name',
+    DB_USER: 'my_username',
+    DB_PASSWORD: 'my_password',
+    DB_HOST: 'localhost',
+    DB_CHARSET: 'utf8',
+    DB_COLLATE: '',
+    table_prefix: 'wp_',
+    WP_DEBUG: false,
+}
